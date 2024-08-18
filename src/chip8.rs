6dd53e0f0b006ffe107b7c47b10cbbf0d51e2455
Tarun Verghis <tarun.verghis@gmail.com>
2024-08-18 10:13:22 -0700

    Load ROM from file

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,9 +1,11 @@
+const MEMORY_SIZE_BYTES: usize = 4096;
 const PROG_CTR_START_ADDR: u16 = 0x200;
+const MAX_ROM_SIZE_BYTES: usize = MEMORY_SIZE_BYTES - 0x200;
 
 #[derive(Debug, Clone)]
 pub struct Chip8 {
     pub registers: [u8; 16],
-    pub memory: [u8; 4096],
+    pub memory: [u8; MEMORY_SIZE_BYTES],
     // `index` needs to hold the maximum possible address in `memory`
     pub index: u16,
     // `program_counter` needs to hold the maximum possible address in `memory`
@@ -34,6 +36,34 @@ impl Default for Chip8 {
     }
 }
 
+impl Chip8 {
+    pub fn load_rom_from_file(&mut self, file_name: &str) -> anyhow::Result<()> {
+        // TODO: don't read the file into memory if it's too large
+        let file_bytes = std::fs::read(file_name)?;
+        self.load_rom_bytes(&file_bytes)
+    }
+
+    fn load_rom_bytes(&mut self, bytes: &[u8]) -> anyhow::Result<()> {
+        let nbytes = bytes.len();
+
+        if nbytes == 0 || nbytes > MAX_ROM_SIZE_BYTES {
+            anyhow::bail!(
+                "rom length is invalid. Received {} bytes, expected between {} and {} bytes.",
+                nbytes,
+                0,
+                MAX_ROM_SIZE_BYTES
+            );
+        }
+
+        let src_copy_range =
+            (PROG_CTR_START_ADDR as usize)..(PROG_CTR_START_ADDR as usize + nbytes);
+
+        self.memory[src_copy_range].copy_from_slice(bytes);
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -43,5 +73,42 @@ mod test {
         let c = Chip8::default();
 
         assert_eq!(c.program_counter, PROG_CTR_START_ADDR);
+    }
+
+    #[test]
+    fn acceptable_rom_sizes() {
+        let mut c = Chip8::default();
+        let rom1 = [0u8; 1];
+        let rom2 = [0u8; MAX_ROM_SIZE_BYTES];
+
+        assert!(c.load_rom_bytes(&rom1).is_ok());
+        assert!(c.load_rom_bytes(&rom2).is_ok());
+    }
+
+    #[test]
+    fn unacceptable_rom_sizes() {
+        let mut c = Chip8::default();
+        let rom1 = [0u8; 0];
+        let rom2 = [0u8; MAX_ROM_SIZE_BYTES + 1];
+
+        assert!(c.load_rom_bytes(&rom1).is_err());
+        assert!(c.load_rom_bytes(&rom2).is_err());
+    }
+
+    #[test]
+    fn load_rom() {
+        let mut c = Chip8::default();
+        const ROM_LEN: usize = MAX_ROM_SIZE_BYTES - 10;
+        let rom = [8u8; ROM_LEN];
+
+        c.load_rom_bytes(&rom).unwrap();
+
+        // For the length of `rom`, the bytes in `memory` should be equal to `rom`.
+        assert_eq!(c.memory[0x200..(0x200 + rom.len())], rom);
+        // The remaining bytes in `memory` should be all zeroes since they should not have been touched.
+        assert_eq!(
+            c.memory[0x200 + rom.len()..],
+            [0; MAX_ROM_SIZE_BYTES - ROM_LEN]
+        );
     }
 }
