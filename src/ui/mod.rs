8ccd959ae3d3082a6d79da404903bd7f8a1b0c66
Tarun Verghis <tarun.verghis@gmail.com>
2024-09-15 21:00:37 -0700

    Show mem index

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,4 @@
-use eframe::egui;
+use eframe::egui::{self, Color32, RichText};
 
 use crate::chip8::Chip8;
 
@@ -28,15 +28,23 @@ impl DebugInterface {
 impl eframe::App for DebugInterface {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Memory");
+            ui.monospace("Memory".to_uppercase());
             egui::ScrollArea::vertical().show(ui, |ui| {
-                for chunk in self.chip8.memory.chunks(16) {
+                for (idx, chunk) in self.chip8.memory.chunks(16).enumerate() {
                     let bytes_string = chunk
                         .iter()
                         .map(|byte| format!("{:02x}", byte))
                         .collect::<Vec<String>>()
                         .join(" ");
-                    ui.monospace(bytes_string);
+                    ui.horizontal(|ui| {
+                        let mem_idx_str = format!("{:03x}", 16 * idx);
+                        ui.label(
+                            RichText::new(mem_idx_str)
+                                .color(Color32::from_rgb(0xA0, 0xDB, 0x8E))
+                                .monospace(),
+                        );
+                        ui.monospace(bytes_string);
+                    });
                 }
             });
         });
